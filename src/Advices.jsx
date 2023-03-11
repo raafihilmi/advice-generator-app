0a70307bf1f966a15f35a750c799e05e4e44689c
Raafi Hilmi <raafihilmi90@gmail.com>
2023-03-11 18:45:38 +0700

    first commit

---
+++ src/Advices.jsx
@@ -0,0 +1,44 @@
+import dividerMobile from './assets/images/pattern-divider-mobile.svg';
+import dividerDesktop from './assets/images/pattern-divider-desktop.svg';
+import dice from './assets/images/icon-dice.svg';
+import { useEffect, useState } from 'react';
+
+export const Advices = () => {
+  const [text, setText] = useState(
+    `"It's easy to sit up and take notice, what's difficult is getting up and taking action."`
+  );
+
+  // useEffect(() => {
+  //   fetch('https://api.adviceslip.com/advice')
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setAdvices(data.slip.advice);
+  //     })
+  //     .catch((err) => console.log(err));
+  // }, []);
+
+  const handleClick = () => {
+    fetch('https://api.adviceslip.com/advice')
+      .then((res) => res.json())
+      .then((data) => {
+        setText(`"${data.slip.advice}"`);
+        console.log(data.slip.advice);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className='container'>
+      <blockquote>
+        <p className='span'>
+          <span>advice #117</span>
+        </p>
+        <p className='advice'>{text}</p>
+        <div className='divider'></div>
+        <button className='btn' onClick={handleClick}>
+          <img src={dice} />
+        </button>
+      </blockquote>
+    </div>
+  );
+};
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Advices } from './Advices';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Advices />
+    </div>
+  );
+}
+
+export default App;
